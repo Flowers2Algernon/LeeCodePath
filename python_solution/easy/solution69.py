e03de3074e32788330326317f4d2f1c758d3eb69
jinhongzhu <zjhong6666@gmail.com>
2024-12-29 21:41:25 +0800

    update method2 for 69py

--- python_solution/easy/solution69.py
+++ python_solution/easy/solution69.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def mySqrt(self, x):
+    def mySqrt1(self, x):
         """
         :type x: int
         :rtype: int
@@ -12,3 +12,20 @@ class Solution(object):
         dif_1 = x - (i - 1) * (i - 1)
         dif_2 = (i+1) * (i+1) - x
         return i - 1 if dif_1 < dif_2 else i
+    def mySqrt2(self ,x):
+        left = 1
+        right = x
+        while left < right:
+            middle = int((left + right) / 2)
+            if middle * middle == x:
+                return middle
+            elif middle * middle < x:
+                left = middle +1
+            else:
+                right = middle - 1
+        # be mind, the result is round down
+        return right
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.mySqrt2(5))
