c490e1b6c93193f37dd743e103754bcf659d23f8
jinhongzhu <zjhong6666@gmail.com>
2024-12-30 20:58:26 +0800

    update solution 283 py

---
+++ python_solution/easy/solution283.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        fast = 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        while slow < len(nums):
+            nums[slow] = 0
+            slow += 1
+        return nums
